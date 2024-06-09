dd3b97051e01bcf6ef3a7f5c14f76f7f4babb267
meeusnietmeus <r0881427@student.thomasmore.be>
2024-06-09 20:55:09 +0200

    projectennnn

---
+++ src/lib/components/project.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import ArrowDownRightIcon from "$lib/icons/arrow-down-right-icon.svelte";
+    import { twMerge } from "tailwind-merge";
+
+    type ComponentProps = {
+        title: string;
+        introduction: string;
+        class: string;
+    };
+
+    const { title, introduction, class: classname }: ComponentProps = $props();
+</script>
+
+<div
+    class={twMerge(
+        "border border-primary-muted/20 flex flex-col rounded-full justify-center items-center group hover:bg-accent transition-colors",
+        classname,
+    )}
+>
+    <h3 class="text-primary text-3xl group-hover:hidden transition">
+        {title}
+    </h3>
+    <p class="text-primary-muted group-hover:hidden max-w-[60%] text-center">
+        {introduction}
+    </p>
+    <ArrowDownRightIcon
+        class="hidden group-hover:block -rotate-90 text-background w-40 h-40"
+    />
+</div>
--- src/routes/projects-section.svelte
+++ src/routes/projects-section.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import Project from "$lib/components/project.svelte";
     import { createTranslator } from "$lib/i18n/i18n";
 
     type ComponentProps = {
@@ -11,10 +12,34 @@
 
 <section id="projects">
     <h2 class="text-accent text-5xl font-black">
-        {translator.get("pages.home.projects.title_1")}
-        <span class="underline"
-            >{translator.get("pages.home.projects.title_2")}</span
-        >
+        {translator.get("pages.home.projects.title")}
     </h2>
-    <p class="mt-3">{translator.get("pages.home.projects.subtitle")}</p>
+    <div class="flex items-center mt-3 mb-8 gap-2">
+        <p>{translator.get("pages.home.projects.filter_1")}</p>
+        <p>{translator.get("pages.home.projects.filter_2")}</p>
+    </div>
+    <div class="grid grid-cols-4 grid-rows-2 w-full h-[70vh] gap-5">
+        <Project
+            title={translator.get("pages.home.projects.internship.title")}
+            introduction={translator.get(
+                "pages.home.projects.internship.introduction",
+            )}
+            class="row-span-2 col-span-2"
+        />
+        <Project
+            title={translator.get("pages.home.projects.project-4.title")}
+            introduction={translator.get(
+                "pages.home.projects.project-4.introduction",
+            )}
+            class=""
+        />
+        <div></div>
+        <Project
+            title={translator.get("pages.home.projects.angular.title")}
+            introduction={translator.get(
+                "pages.home.projects.angular.introduction",
+            )}
+            class="col-span-2"
+        />
+    </div>
 </section>
