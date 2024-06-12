2deb23a1ad51f294ec340f79e44c79133c74ca30
meeusnietmeus <r0881427@student.thomasmore.be>
2024-06-12 15:48:12 +0200

    finallll

--- src/routes/projects/angular/+page.svelte
+++ src/routes/projects/angular/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-    import nextjsLogo from "$lib/images/logos/nextjs.webp";
+    import angularLogo from "$lib/images/logos/angular.webp";
+    import csharpNetLogo from "$lib/images/logos/csharp_net.webp";
     import Pill from "$lib/components/pill.svelte";
     import CalendarIcon from "$lib/icons/calendar-icon.svelte";
     import UsersIcon from "$lib/icons/users-icon.svelte";
-    import homepageImage from "$lib/images/projects/project-4/home.webp";
-    import dashboardImage from "$lib/images/projects/project-4/dashboard.webp";
-    import chatImage from "$lib/images/projects/project-4/chat.webp";
+    import landingImage from "$lib/images/projects/angular/landing.webp";
+    import tripImage from "$lib/images/projects/angular/trip.webp";
+    import manageImage from "$lib/images/projects/angular/manage.webp";
+    import winnerImage from "$lib/images/projects/angular/winner.webp";
     import type { LayoutData } from "../../$types";
     import { createTranslator } from "$lib/i18n/i18n";
 
@@ -24,20 +26,20 @@
     <div class="max-w-96 flex flex-col justify-between">
         <div class="space-y-3">
             <h2 class="text-accent text-5xl font-black">
-                {translator.get("pages.project-4.title")}
+                {translator.get("pages.angular.title")}
             </h2>
             <div class="flex gap-4 flex-wrap">
                 <Pill>
                     <UsersIcon class="w-4" />
-                    <p>{translator.get("pages.project-4.group-members")}</p>
+                    <p>{translator.get("pages.angular.group-members")}</p>
                 </Pill>
                 <Pill>
                     <CalendarIcon class="w-4" />
-                    <p>{translator.get("pages.project-4.date")}</p>
+                    <p>{translator.get("pages.angular.date")}</p>
                 </Pill>
             </div>
             <p>
-                {translator.get("pages.project-4.description")}
+                {translator.get("pages.angular.description")}
             </p>
         </div>
 
@@ -50,29 +52,39 @@
     <div class="space-y-5">
         <div class="flex gap-16 px-10 lg:py-5 py-2">
             <div class="flex gap-4 items-center">
-                <img src={nextjsLogo} alt="Logo" width="20" height="20" />
-                <p class="text-primary-muted/70 font-bold">Next.js</p>
+                <img src={angularLogo} alt="Logo" width="20" height="20" />
+                <p class="text-primary-muted/70 font-bold">Angular</p>
+            </div>
+            <div class="flex gap-4 items-center">
+                <img src={csharpNetLogo} alt="Logo" width="22" height="20" />
+                <p class="text-primary-muted/70 font-bold">.NET</p>
             </div>
         </div>
         <div
-            class="bg-primary-muted/15 rounded-3xl px-10 py-14 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 place-items-center gap-4"
+            class="bg-primary-muted/15 rounded-3xl px-10 py-14 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 place-items-center gap-4"
         >
             <img
-                src={homepageImage}
+                src={landingImage}
                 alt="challenge"
-                class="rounded-xl col-span-2"
+                class="rounded-xl"
                 title="challenge details"
             />
             <img
-                src={chatImage}
+                src={tripImage}
                 alt="profile"
-                class="rounded-xl row-span-2"
+                class="rounded-xl"
                 title="profile"
             />
             <img
-                src={dashboardImage}
+                src={manageImage}
                 alt="create challenge"
-                class="rounded-xl col-span-2"
+                class="rounded-xl"
+                title="creating a new challenge"
+            />
+            <img
+                src={winnerImage}
+                alt="create challenge"
+                class="rounded-xl"
                 title="creating a new challenge"
             />
         </div>
@@ -86,18 +98,18 @@
     <div class="space-y-12 lg:pt-32 order-2 lg:order-1">
         <section class="max-w-reading">
             <h3 class="text-accent text-3xl font-black">
-                {translator.get("pages.project-4.section-1.title")}
+                {translator.get("pages.angular.section-1.title")}
             </h3>
             <p class="mt-3">
-                {translator.get("pages.project-4.section-1.text")}
+                {translator.get("pages.angular.section-1.text")}
             </p>
         </section>
         <section class="max-w-reading">
             <h3 class="text-accent text-3xl font-black">
-                {translator.get("pages.project-4.section-2.title")}
+                {translator.get("pages.angular.section-2.title")}
             </h3>
             <p class="mt-3">
-                {translator.get("pages.project-4.section-2.text")}
+                {translator.get("pages.angular.section-2.text")}
             </p>
         </section>
     </div>
@@ -107,19 +119,15 @@
     >
         <div class="bg-primary-muted/10 py-2 px-4 rounded-xl">
             <h4 class="text-xl font-semibold text-accent">
-                {translator.get("pages.project-4.cards.1.title")}
+                {translator.get("pages.angular.cards.1.title")}
             </h4>
-            <p>{translator.get("pages.project-4.cards.1.text")}</p>
+            <p>{translator.get("pages.angular.cards.1.text")}</p>
         </div>
         <div class=" bg-primary-muted/10 py-2 px-4 rounded-xl">
             <h4 class="text-xl font-semibold text-accent">
-                {translator.get("pages.project-4.cards.2.title")}
+                {translator.get("pages.angular.cards.2.title")}
             </h4>
-            <p>{translator.get("pages.project-4.cards.2.text-1")}</p>
-            <p>{translator.get("pages.project-4.cards.2.text-2")}</p>
-            <p>{translator.get("pages.project-4.cards.2.text-3")}</p>
-            <p>{translator.get("pages.project-4.cards.2.text-4")}</p>
-            <p>{translator.get("pages.project-4.cards.2.text-5")}</p>
+            <p>{translator.get("pages.angular.cards.2.text-1")}</p>
         </div>
     </aside>
 </article>
